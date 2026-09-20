c79c880ab31c6fe517029a162ded030c8cab389a
mrcodelover1211 <zak07052016@gmail.com>
2026-09-20 15:58:45 +0100

    Make airport tower icons larger and easier to press

--- app.js
+++ app.js
@@ -596,9 +596,9 @@ function drawRoute(f){
 function airportTowerIcon(){
   return L.divIcon({
     className:"airport-tower-wrap",
-    html:'<div class="airport-tower" aria-label="Airport"><svg viewBox="0 0 20 20"><path d="M3 18h14M5 18V7h10v11M4 7l6-5 6 5M7 10h2v2H7zm4 0h2v2h-2zm-4 4h2v2H7zm4 0h2v2h-2z"/></svg></div>',
-    iconSize:[16,16],
-    iconAnchor:[8,8]
+    html:'<div class="airport-tower" aria-label="Airport"><div class="airport-tower-hit"><svg viewBox="0 0 20 20"><path d="M3 18h14M5 18V7h10v11M4 7l6-5 6 5M7 10h2v2H7zm4 0h2v2h-2zm-4 4h2v2H7zm4 0h2v2h-2z"/></svg></div></div>',
+    iconSize:[44,44],
+    iconAnchor:[22,22]
   });
 }
 
